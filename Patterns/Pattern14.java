6f312188fadbf3f2ccd0c4d4954ad38154b7c21a
UltronIsAlive <mohanvbb9899@gmail.com>
2022-02-06 17:28:46 +0530

    Pattern 14

---
+++ Patterns/Pattern14.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Pattern14 {
+	public static void pattern(int n) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(n + " * " + i + " = " + (n * i));
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		pattern(n);
+
+	}
+}
