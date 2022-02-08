d751b700a034d4440ae3e9a24e5474468c793235
UltronIsAlive <mohanvbb9899@gmail.com>
2022-02-08 11:28:41 +0530

    Pattern 20

---
+++ Patterns/Pattern20.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Pattern20 {
+    public static void pattern(int n) {
+        for (int r = 1; r <= n; r++) {
+            for (int c = 1; c <= n; c++) {
+                if(c==1 || c==n)
+                    System.out.print("*\t");
+                else if(r>n/2 && ((r==c) || (r+c == n+1)))
+                    System.out.print("*\t");
+                else
+                    System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		pattern(n);
+    }
+}
