8ab85a4565f75a8fb0b403a9e7b7a01403925c60
UltronIsAlive <mohanvbb9899@gmail.com>
2022-02-08 18:51:12 +0530

    Pattern Finished

--- Patterns/PatternM.java
+++ Patterns/PatternM.java
@@ -1,12 +1,12 @@
 import java.util.*;
 
-public class Pattern20 {
+public class PatternM {
 	public static void pattern(int n) {
 		for (int r = 1; r <= n; r++) {
 			for (int c = 1; c <= n; c++) {
 				if (c == 1 || c == n)
 					System.out.print("*\t");
-				else if (r > n / 2 && ((r == c) || (r + c == n + 1)))
+				else if (r <= n / 2 + 1 && ((r == c) || (r + c == n + 1)))
 					System.out.print("*\t");
 				else
 					System.out.print("\t");
