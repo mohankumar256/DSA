a42e5c7eb66318c8d0ceb56c993e961c881b2c53
UltronIsAlive <mohanvbb9899@gmail.com>
2022-02-10 20:26:46 +0530

    Decimal To Any Base

---
+++ Functions and Arrays/DecimalToAnyBase.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class DecimalToAnyBase {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int b = scn.nextInt();
+        int dn = getValueInBase(n, b);
+        System.out.println(dn);
+    }
+
+    public static int getValueInBase(int n, int b) {
+        int res = 0, pwr = 1;
+        while (n != 0) {
+            int rem = n % b; // remainder 
+            n /= b; // dividing number by given base
+
+            res += rem * pwr; 
+
+            pwr *= 10;
+        }
+        return res;
+    }
+}
