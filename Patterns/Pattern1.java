3c731306d7b5f52bb65b5d2a3a0871130aa42e31
UltronIsAlive <mohanvbb9899@gmail.com>
2022-02-12 20:22:36 +0530

    .

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -4,8 +4,8 @@ public class Pattern1 {
 	public static void pattern(int n) {
 		int nst = 1; // nst=number of stars
 		for (int r = 1; r <= n; r++) {
-			for (int cst = 0; cst < nst; cst++) //count of stars
-				System.out.print("*\t");
+			for (int cst = 0; cst < nst; cst++) 
+				System.out.print("*\t"); //count of stars
 
 			nst++;
 			System.out.println();
