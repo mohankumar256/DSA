2b75ac22748e4bc760f32cb3c088229eff3a25b8
UltronIsAlive <mohanvbb9899@gmail.com>
2022-02-12 19:33:48 +0530

    Any Base to Decimal

--- Functions and Arrays/AnyBaseToDecimal.java
+++ Functions and Arrays/AnyBaseToDecimal.java
@@ -1,3 +1,23 @@
+import java.util.*;
+
 public class AnyBaseToDecimal {
-	
+
+   public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int b = scn.nextInt();
+      int d = getValueIndecimal(n, b);
+      System.out.println(d);
+   }
+
+   public static int getValueIndecimal(int n, int b) {
+      int ans = 0, pwr = 1;
+      while (n != 0) {
+         int lastDigit = n % 10;
+         n /= 10;
+         ans += lastDigit * pwr;
+         pwr *= b;
+      }
+      return ans;
+   }
 }
