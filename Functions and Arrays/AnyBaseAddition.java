1b85c41f2c841b62145c3a9e97b94c401edcfc9b
UltronIsAlive <mohanvbb9899@gmail.com>
2022-02-13 22:52:47 +0530

    Any Base Addition

---
+++ Functions and Arrays/AnyBaseAddition.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class AnyBaseAddition {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int b = scn.nextInt();
+		int n1 = scn.nextInt();
+		int n2 = scn.nextInt();
+
+		int d = getSum(b, n1, n2);
+		System.out.println(d);
+	}
+
+	public static int getSum(int b, int n1, int n2) {
+		int carry = 0, ans = 0, pwr = 1;
+		while (n1 != 0 || n2 != 0 || carry != 0) {
+			int sum = n1 % 10 + n2 % 10 + carry;
+
+			n1 /= 10;
+			n2 /= 10;
+
+			carry = sum / b;
+			sum %= b;
+
+			ans += sum * pwr;
+			pwr *= 10;
+		}
+		return ans;
+	}
+}
