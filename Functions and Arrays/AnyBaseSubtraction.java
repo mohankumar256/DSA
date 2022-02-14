703d9837f044a3187f36d564c2fd0de0f15dea30
UltronIsAlive <mohanvbb9899@gmail.com>
2022-02-14 20:45:33 +0530

    Any Base Subtraction

--- Functions and Arrays/AnyBaseSubtraction.java
+++ Functions and Arrays/AnyBaseSubtraction.java
@@ -1,3 +1,36 @@
+import java.util.*;
+
 public class AnyBaseSubtraction {
-	
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int b = scn.nextInt();
+		int n1 = scn.nextInt();
+		int n2 = scn.nextInt();
+
+		int d = getDifference(b, n1, n2);
+		System.out.println(d);
+	}
+
+	public static int getDifference(int b, int n1, int n2) {
+		int ans = 0, pwr = 1, carry = 0;
+		while (n2 != 0) {
+			int diff = n2 % 10 - n1 % 10 - carry;
+			n2 /= 10;
+			n1 /= 10;
+
+			if (diff < 0) {
+				carry = 1;
+				diff += b;
+			} else {
+				carry = 0;
+			}
+
+			ans += diff * pwr;
+			pwr *= 10;
+
+		}
+		return ans;
+	}
+
 }
