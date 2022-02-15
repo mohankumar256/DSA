4c8c50bf1196f11bd7962d46e056e129c994b619
UltronIsAlive <mohanvbb9899@gmail.com>
2022-02-15 23:13:47 +0530

    span of array

--- Functions and Arrays/SpanOfArray.java
+++ Functions and Arrays/SpanOfArray.java
@@ -1,14 +1,18 @@
+import java.io.*;
 import java.util.*;
 
 public class SpanOfArray {
 
 	public static int spanOfArray(int[] arr) {
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
+		int max = arr[0];
+		int min = arr[0];
 
-		for (int i = 0; i < arr.length; i++) {
-			max = Math.max(max, arr[i]);
-			min = Math.min(min, arr[i]);
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > max)
+				max = arr[i];
+
+			if (arr[i] < min)
+				min = arr[i];
 		}
 		return (max - min);
 	}
@@ -17,10 +21,10 @@ public class SpanOfArray {
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
 		int[] arr = new int[n];
-		for (int i = 0; i < n; i++)
+		for (int i = 0; i < arr.length; i++)
 			arr[i] = scn.nextInt();
 
 		System.out.println(spanOfArray(arr));
-
 	}
+
 }
