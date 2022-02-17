56078720d55dfcebc650cca28d0adbb5454756d9
UltronIsAlive <mohanvbb9899@gmail.com>
2022-02-17 15:43:09 +0530

    Getting Started Finished

--- Getting Started/BenjaminBulbs.java
+++ Getting Started/BenjaminBulbs.java
@@ -4,7 +4,7 @@ public class BenjaminBulbs {
 	public static void benjaminBulbs(int num) {
 		for (int i = 1; i * i <= num; i++)
 			System.out.println(i * i); // print perfect squares
-	}
+	} // On all odd fluctuation bulb glows
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
