0a238a2819efc45089ad456bd2e87354dbb10f50
UltronIsAlive <mohanvbb9899@gmail.com>
2022-02-17 15:44:07 +0530

    Patterns Finished

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Pattern1 {
 	public static void pattern(int n) {
-		int nst = 1; // nst=number of stars
+		int nst = 1; // nst = number of stars
 		for (int r = 1; r <= n; r++) {
-			for (int cst = 0; cst < nst; cst++) 
-				System.out.print("*\t"); //count of stars
+			for (int cst = 0; cst < nst; cst++)
+				System.out.print("*\t"); // cst = count of stars
 
 			nst++;
 			System.out.println();
