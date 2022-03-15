c67c0bb011bdaf175d0d211cb4aed94bf28aca23
Mohan Kumar <mohanvbb9899@gmail.com>
2022-03-15 05:55:18 +0530

    factorial

---
+++ Recursion and Backtracking/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String[] args) throws Exception {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int f = factorial(n);
+		System.out.println(f);
+	}
+
+	public static int factorial(int n) {
+		if (n == 1)
+			return 1;
+
+		int fac = n * (factorial(n - 1));
+
+		return fac;
+	}
+}
---
+++ Recursion and Backtracking/PrintIncreasingDecreasing.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintIncreasingDecreasing {
+	public static void main(String[] args) throws Exception {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		pdi(n);
+	}
+
+	public static void pdi(int n) {
+		if (n == 0)
+			return;
+
+		System.out.println(n);
+		pdi(n - 1);
+		System.out.println(n);
+	}
+}
