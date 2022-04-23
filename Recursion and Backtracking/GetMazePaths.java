25722f7cf738f72bffe9b0e1e1c565693acc6d53
Mohan Kumar <mohanvbb9899@gmail.com>
2022-04-23 19:30:25 +0530

    get maze path

---
+++ Recursion and Backtracking/GetMazePaths.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class GetMazePaths {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        ArrayList<String> paths = getMazePaths(1, 1, n, m);
+        System.out.println(paths);
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+        if(sr == dr && sc == dc){
+            ArrayList<String> bres = new ArrayList<String>();
+            bres.add("");
+            return bres;
+        }
+
+        ArrayList<String> hpaths = new ArrayList<String>();
+        ArrayList<String> vpaths = new ArrayList<String>();
+
+        if(sc < dc){
+            hpaths = getMazePaths(sr, sc+1, dr, dc);
+        }
+        if(sr < dr){
+            vpaths = getMazePaths(sr+1, sc, dr, dc);
+        }
+
+        ArrayList<String> paths = new ArrayList<String>();
+
+        for (String hpath : hpaths) {
+            paths.add("h" + hpath);
+        }
+
+        for (String vpath : vpaths) {
+            paths.add("v" + vpath);
+        }
+        return paths;
+    }
+
+}
